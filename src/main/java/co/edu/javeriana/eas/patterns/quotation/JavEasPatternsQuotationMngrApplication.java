0705a843132e73ea1fff7a98ee21ab0205ef827a
ingAndresMartinez <ing.andresmartinezcobos@gmail.com>
2020-04-05 15:27:27 -0500

    Add java project

---
+++ src/main/java/co/edu/javeriana/eas/patterns/quotation/JavEasPatternsQuotationMngrApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.eas.patterns.quotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavEasPatternsQuotationMngrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavEasPatternsQuotationMngrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/eas/patterns/quotation/JavEasPatternsQuotationMngrApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.eas.patterns.quotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavEasPatternsQuotationMngrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
